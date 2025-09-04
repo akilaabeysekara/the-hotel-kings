debabc13604e2c801a899201c4028e1e0bc9dca8
akilaabeysekara <akila2024e@gmail.com>
2025-09-04 18:28:05 +0530

    fix gallery section

--- js/gallery.js
+++ js/gallery.js
@@ -1,50 +1,56 @@
 const images = [
-                "assets/images/hotel-img1.jpg",
-                "assets/images/hotel-img2.jpg",
-                "assets/images/hotel-img3.jpg",
-                "assets/images/hotel-img4.jpg",
-                "assets/images/hotel-img5.jpg",
-                "assets/images/hotel-img9.jpg"
-            ];
+  "assets/images/hotel-img1.jpg",
+  "assets/images/hotel-img2.jpg",
+  "assets/images/hotel-img3.jpg",
+  "assets/images/hotel-img4.jpg",
+  "assets/images/hotel-img5.jpg",
+  "assets/images/hotel-img9.jpg"
+];
 
-            let currentIndex = 1;
-            let autoPlayInterval;
+let currentIndex = 1;
+let autoPlayInterval;
 
-            function switchGallery(index) {
-                currentIndex = index;
-                updateGallery();
-            }
+function switchGallery(index) {
+  currentIndex = index;
+  updateGallery();
+}
 
-            function changeImage(step) {
-                currentIndex += step;
-                if (currentIndex > images.length) currentIndex = 1;
-                if (currentIndex < 1) currentIndex = images.length;
-                updateGallery();
-            }
+function changeImage(step) {
+  currentIndex += step;
+  if (currentIndex > images.length) currentIndex = 1;
+  if (currentIndex < 1) currentIndex = images.length;
+  updateGallery();
+}
 
-            function updateGallery() {
-                const photo = document.getElementById("active-photo");
+function updateGallery() {
+  const photo = document.getElementById("active-photo");
 
-                photo.classList.add("fade-out");
+  photo.classList.add("fade-out");
+  setTimeout(() => {
+    photo.src = images[currentIndex - 1];
+    photo.classList.remove("fade-out");
+  }, 250);
 
-                setTimeout(() => {
-                    photo.src = images[currentIndex - 1];
+  document
+    .querySelectorAll(".gallery-pagination .page-item")
+    .forEach((el) => el.classList.remove("active"));
+  document
+    .querySelectorAll(".gallery-pagination .page-item")
+    [currentIndex - 1].classList.add("active");
+}
 
-                    photo.classList.remove("fade-out");
-                }, 250);
+function startAutoPlay() {
+  stopAutoPlay(); 
+  autoPlayInterval = setInterval(() => {
+    changeImage(1);
+  }, 3000);
+}
 
-                document.querySelectorAll(".gallery-pagination .page-item").forEach(el => el.classList.remove("active"));
-                document.querySelectorAll(".gallery-pagination .page-item")[currentIndex - 1].classList.add("active");
-            }
+function stopAutoPlay() {
+  if (autoPlayInterval) {
+    clearInterval(autoPlayInterval);
+    autoPlayInterval = null;
+  }
+}
 
-            function startAutoPlay() {
-                autoPlayInterval = setInterval(() => {
-                    changeImage(1);
-                }, 3000);
-            }
-
-            function stopAutoPlay() {
-                clearInterval(autoPlayInterval);
-            }
-
-            window.onload = startAutoPlay;
+window.onload = startAutoPlay;
